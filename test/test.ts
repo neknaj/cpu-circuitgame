fd0be69f8c54c44f446e0138b4df59d78f2d27cb
bem130 <bem130@ymail.ne.jp>
2025-01-13 13:39:48 +0900

    norのテスト

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,11 @@
 import { Modules } from "./module/small_stack_machine.js";
 
-console.log("hello");
-console.log(Modules);
+// nor
+for (let a of [true,false]) {
+    for (let b of [true,false]) {
+        console.assert(Modules.nor.inputsLen==2,`[func type] nor`);
+        console.assert(Modules.nor.outputsLen==1,`[func type] nor`);
+        // console.log("nor",a,b," ",...Modules.nor.func(a,b));
+        console.assert(Modules.nor.func(a,b)[0]==(!(a|b)),`[func type] nor`,a,b,":",Modules.nor.func(a,b)[0],!(a|b));
+    }
+}
